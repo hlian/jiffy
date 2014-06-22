8320c0b0dddd5128d9c5705ba4aec52c9c4019d0
Hao Lian <me@haolian.org>
2014-06-22 18:21:29 -0400

    xcode!

---
+++ Jiffy/jiffy.h
@@ -0,0 +1,13 @@
+//
+//  jiffy.h
+//  jiffy
+//
+//  Created by hao on 6/22/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface jiffy : NSObject
+
+@end
---
+++ Jiffy/jiffy.m
@@ -0,0 +1,13 @@
+//
+//  jiffy.m
+//  jiffy
+//
+//  Created by hao on 6/22/14.
+//
+//
+
+#import "jiffy.h"
+
+@implementation jiffy
+
+@end
